78527d451b63b108fa2fe00cd37d8e5315747b50
Velmoren <velmoren@yahoo.com>
2024-08-15 12:01:06 +0300

    add 3 sections

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
-import './src/styles/index.scss'
-import javascriptLogo from '/images/javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './src/scripts/modules/counter.js'
+import './src/scss/index.scss';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+import '/node_modules/bootstrap/js/src/base-component.js'
+import '/node_modules/bootstrap/js/src/popover.js'
+import '/node_modules/bootstrap/js/src/collapse.js'
+import '/node_modules/bootstrap/js/src/button.js'
+import '/node_modules/bootstrap/js/src/dropdown.js'
