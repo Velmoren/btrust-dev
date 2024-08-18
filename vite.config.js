ba38ac76eb3021bd85cb657212a8729604952db3
Velmoren <velmoren@yahoo.com>
2024-08-18 16:52:00 +0300

    change list page

--- vite.config.js
+++ vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, '404.html'),
         faq: resolve(__dirname, 'faq.html'),
         forex: resolve(__dirname, 'forex.html'),
         program: resolve(__dirname, 'program.html'),
         futures: resolve(__dirname, 'futures.html'),
         contact: resolve(__dirname, 'contact.html'),
+        404: resolve(__dirname, '404.html'),
       },
     },
   },
